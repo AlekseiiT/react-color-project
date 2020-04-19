a3acbc0fe0b3791c1a1e7e6f183bb7ad9d78ea18
AlexeyTarasovv <coolerr195@gmail.com>
2020-04-19 12:48:15 +0300

    style ColorBox

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Palette {...seedColors[0]} />
+				<Palette {...seedColors[1]} />
 			</div>
 		);
 	}
