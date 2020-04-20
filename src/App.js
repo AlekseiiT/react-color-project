f03d73d7a3c968d823c893099aa8a84d76180fcd
AlexeyTarasovv <coolerr195@gmail.com>
2020-04-20 17:45:34 +0300

    add react-router-dom and generate /palette/:id route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
+import { Route, Switch } from 'react-router-dom';
 
 export default class App extends Component {
+	findPalette(id) {
+		return seedColors.find((el) => el.id === id);
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<Palette palette={generatePalette(seedColors[4])} />
+				<Switch>
+					<Route exact path="/" render={() => <h1>Palette goes here</h1>} />
+					<Route
+						exact
+						path="/palette/:id"
+						render={(routeProps) => (
+							<Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+						)}
+					/>
+				</Switch>
+
+				{console.log(generatePalette(seedColors[4]))}
+				{/* <Palette palette={generatePalette(seedColors[4])} /> */}
 			</div>
 		);
 	}
